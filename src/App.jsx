22304d6edf17d6d4971b93518fe06efb3ba7b700
summerwavesounds-creator <summerwavesounds@gmail.com>
2026-06-08 00:09:07 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ArtistSection from './ArtistSection.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>SummerWave Sounds</h1>
+      </header>
+      <main>
+        <ArtistSection />
+      </main>
+    </div>
+  );
+}
+
+export default App;
