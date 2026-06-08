2ef70e5d3affcb5a3af1259f41a100a3193c640c
summerwavesounds-creator <summerwavesounds@gmail.com>
2026-06-08 00:52:25 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './style.css' // Changed from './index.css'
+import './style.css' // Ensure this path matches your actual CSS filename
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
