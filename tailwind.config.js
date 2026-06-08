5956d624a0957172ea8ee854726e9ef3177773b1
summerwavesounds-creator <summerwavesounds@gmail.com>
2026-06-07 22:47:48 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.jsx",
+    "./App.jsx",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
