b95e09c7f645bf3e6235ffff1eb766dec49852c5
summerwavesounds-creator <summerwavesounds@gmail.com>
2026-06-07 22:23:20 -0600

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Points to App.jsx in the same root folder
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
