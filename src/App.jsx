46db2237540e25cc224e9a254059bb03e6c702fd
summerwavesounds-creator <summerwavesounds@gmail.com>
2026-06-08 00:33:25 -0600

    Refactor App component to import additional hooks and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import React from 'react';
-import ArtistSection from './ArtistSection.jsx';
-
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>SummerWave Sounds</h1>
-      </header>
-      <main>
-        <ArtistSection />
-      </main>
-    </div>
-  );
-}
-
-export default App;
+import { useState, useEffect, useRef } from "react";
+import {
+  Play, Pause, ChevronDown, Heart, Youtube, Instagram,
+  Twitter, Music2, ArrowRight, Sparkles, Globe, Users,
+  Zap, Headphones, Radio, Star, Menu, X, Volume2,
+  TrendingUp, Coffee, Moon, Sun
+} from "lucide-react";
